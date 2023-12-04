6fda6a5448ea72eb5ed9bfd41c961f4c65a97410
Jonathan Peña <97184743+JJPENA666@users.noreply.github.com>
2023-12-04 09:01:30 -0500

    cambios db

--- lib/src/db/operation.dart
+++ lib/src/db/operation.dart
@@ -1,10 +1,12 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-class Operation{
-
-  Future<Database> _openDB(){
-    return openDatabase(join(getDatabasesPath() as String,"notes.db"));
+class Operation {
+  Future<Database> _openDB() {
+    return openDatabase(join(getDatabasesPath() as String, "notes.db"),
+        onCreate: (db, version) {
+      return db.execute(
+          "CREATE TABLE notes (id INTEGER PRIMARY KEY,title TEXT, content TEXT");
+    }, version: 1);
   }
-
 }
